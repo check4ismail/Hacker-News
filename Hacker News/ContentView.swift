575d60c34a7d6dadf76c94ec1da1eadb94088259
Ismail Elmaliki <check4elmaliki@live.com>
2020-06-29 07:54:16 -0400

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Ismail Elmaliki on 6/29/20.
+//  Copyright © 2020 Ismail Elmaliki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
